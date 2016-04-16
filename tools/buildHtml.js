13c9c188302c224c6facb007bba402c1eff34fc1
Caspar Krieger <git@asparck.com>
2016-04-16 20:34:12 +1000

    Clarify what index.html was created

--- tools/buildHtml.js
+++ tools/buildHtml.js
@@ -25,6 +25,6 @@ fs.readFile('src/index.html', 'utf8', (err, markup) => {
     if (err) {
       return console.log(err);
     }
-    console.log('index.html written to /dist'.green);
+    console.log('Created /dist/index.html'.green);
   });
 });
