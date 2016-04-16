caad5c6fefcb8549cd982c2de7f61c219f92fb77
Caspar Krieger <git@asparck.com>
2016-04-16 20:34:12 +1000

    Remove browser-sync and use webpack dev server directly

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ export default {
   devtool: 'cheap-module-eval-source-map', // more info:https://webpack.github.io/docs/build-performance.html#sourcemaps and https://webpack.github.io/docs/configuration.html#devtool
   noInfo: true, // set to false to see a list of every file being bundled.
   entry: [
-    'webpack-hot-middleware/client?reload=true',
+    'webpack-dev-server/client?http://localhost:3000/',
     './src/index'
   ],
   target: 'web', // necessary per https://webpack.github.io/docs/testing.html#compile-and-test
@@ -21,7 +21,7 @@ export default {
     filename: 'bundle.js'
   },
   plugins: [
-    new webpack.DefinePlugin(GLOBALS), //Tells React to build in prod mode. https://facebook.github.io/react/downloads.htmlnew webpack.HotModuleReplacementPlugin());
+    new webpack.DefinePlugin(GLOBALS), //Tells React to build in prod mode. https://facebook.github.io/react/downloads.html
     new webpack.HotModuleReplacementPlugin(),
     new webpack.NoErrorsPlugin()
   ],
@@ -31,5 +31,13 @@ export default {
       {test: /\.(jpe?g|png|gif|svg)$/i, loaders: ['file']},
       {test: /(\.css|\.scss)$/, loaders: ['style', 'css?sourceMap', 'sass?sourceMap']}
     ]
+  },
+  devServer: {
+    contentBase: __dirname + '/src',
+    historyApiFallback: true,
+    hot: true,
+    noInfo: true,
+    colors: true,
+    port: 3000
   }
 };
