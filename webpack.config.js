80adfe0df6a6c88ab8be25187e35a5a90c044c13
Caspar Krieger <git@asparck.com>
2016-04-16 20:34:12 +1000

    Move static assets to be served from /assets
    In preparation for adding a back-end server

--- webpack.config.js
+++ webpack.config.js
@@ -28,8 +28,8 @@ export default {
   ] : ['./src/index'],
   target: 'web', // necessary per https://webpack.github.io/docs/testing.html#compile-and-test
   output: {
-    path: __dirname + '/dist',
-    publicPath: '/',
+    path: __dirname + '/dist/assets',
+    publicPath: '/assets/',
     filename: 'bundle.js'
   },
   plugins: DEVMODE ? [ // debug plugins
