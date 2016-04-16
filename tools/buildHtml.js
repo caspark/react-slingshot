d120b68bfbd2527b10e90cad166d4f8b0f0945c7
Caspar Krieger <git@asparck.com>
2016-04-16 20:34:12 +1000

    Remove a leftover reference to TrackJS

--- tools/buildHtml.js
+++ tools/buildHtml.js
@@ -1,12 +1,8 @@
-// This script copies src/index.html into /dist/index.html
-// and adds TrackJS error tracking code for use in production
-// when useTrackJs is set to true below and a trackJsToken is provided.
-// This is a good example of using Node and cheerio to do a simple file transformation.
-// In this case, the transformation is useful since we only want to track errors in the built production code.
-
 // Allowing console calls below since this is a build file.
 /*eslint-disable no-console */
 
+// This script copies src/index.html into /dist/index.html and adds a reference to styles.css to the head; it's only run
+// for production builds (and isn't used during local development).
 import fs from 'fs';
 import colors from 'colors';
 import cheerio from 'cheerio';
