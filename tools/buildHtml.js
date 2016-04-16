b09fb779b3ee03e1f3d7e4cb322b8ecedd1426b7
Caspar Krieger <git@asparck.com>
2016-04-16 20:34:12 +1000

    Remove trackJS advertising

--- tools/buildHtml.js
+++ tools/buildHtml.js
@@ -11,9 +11,6 @@ import fs from 'fs';
 import colors from 'colors';
 import cheerio from 'cheerio';
 
-const useTrackJs = true; // If you choose not to use TrackJS, just set this to false and the build warning will go away.
-const trackJsToken = ''; // If you choose to use TrackJS, insert your unique token here. To get a token, go to https://trackjs.com
-
 fs.readFile('src/index.html', 'utf8', (err, markup) => {
   if (err) {
     return console.log(err);
@@ -24,16 +21,6 @@ fs.readFile('src/index.html', 'utf8', (err, markup) => {
   // since a separate spreadsheet is only utilized for the production build, need to dynamically add this here.
   $('head').prepend('<link rel="stylesheet" href="styles.css">');
 
-  if (useTrackJs) {
-    if (trackJsToken) {
-      const trackJsCode = `<!-- BEGIN TRACKJS Note: This should be the first <script> on the page per https://my.trackjs.com/install --><script>window._trackJs = { token: '${trackJsToken}' };</script><script src=https://d2zah9y47r7bi2.cloudfront.net/releases/current/tracker.js></script><!-- END TRACKJS -->`;
-
-      $('head').prepend(trackJsCode); // add TrackJS tracking code to the top of <head>
-    } else {
-      console.log('To track JavaScript errors, sign up for a free trial at TrackJS.com and enter your token in /tools/build.html on line 10.'.yellow);
-    }
-  }
-
   fs.writeFile('dist/index.html', $.html(), 'utf8', function (err) {
     if (err) {
       return console.log(err);
@@ -41,4 +28,3 @@ fs.readFile('src/index.html', 'utf8', (err, markup) => {
     console.log('index.html written to /dist'.green);
   });
 });
-
