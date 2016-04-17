c8354ffeb94bd945812f9051a119c313cd2b8160
Caspar Krieger <git@asparck.com>
2016-04-17 21:19:34 +1000

    Fix broken store initialisation
    While the previous approach type-checked, it didn't actually work.
    
    Woops.

--- src/front/store/configureStore.js
+++ src/front/store/configureStore.js
@@ -1,25 +1,31 @@
 // @flow
+import { createStore, compose } from 'redux';
+import type FuelSavingsState from '../reducers/fuelSavings';
+import rootReducer from '../reducers';
 
-const THIS_FILE='store/configureStore.js';
+/*
+ * Wrap's redux's createStore to hook in the redux developer tools.
+ *
+ * Really, in production, the body of this function should just be "return createStore(rootReducer, initialState);",
+ * however we don't have a nice way of swapping between development and production modes (or at least, we can't do
+ * it using runtime imports of modules, because it seems like Flow doesn't like that) so instead we just keep the
+ * dev-mode code around in production as well.
+ */
+export default function configureStore(initialState: any) {
 
-if (process.env.NODE_ENV === 'production') {
-  // Jumping through some hoops (wrapping the function) to keep flow happy
-  // (it doesn't seem to like mixing dynamic imports, or maybe it's the mixing of ES6 modules with CommonJS modules)
-  module.exports = function() {
-    let real = require('./configureStore.prod');
-    if (typeof real === 'function') {
-      real.apply(this, arguments);
-    } else {
-      throw new TypeError('./configureStore.prod did not export a function', THIS_FILE);
-    }
-  };
-} else {
-  module.exports = function() {
-    let real = require('./configureStore.dev');
-    if (typeof real === 'function') {
-      real.apply(this, arguments);
-    } else {
-      throw new TypeError('./configureStore.dev did not export a function', THIS_FILE);
-    }
-  };
+  let store = createStore(rootReducer, initialState, compose(
+    // Add other middleware on this line...
+    window.devToolsExtension ? window.devToolsExtension() : f => f //add support for Redux dev tools
+    )
+  );
+
+  if (module.hot) {
+    // Enable Webpack hot module replacement for reducers
+    module.hot.accept('../reducers', () => {
+      const nextReducer = require('../reducers').default;
+      store.replaceReducer(nextReducer);
+    });
+  }
+
+  return store;
 }
--- src/front/store/configureStore.prod.js
+++
@@ -1,8 +0,0 @@
-// @flow
-import { createStore } from 'redux';
-import type FuelSavingsState from '../reducers/fuelSavings';
-import rootReducer from '../reducers';
-
-export default function configureStore(initialState: FuelSavingsState) {
-  return createStore(rootReducer, initialState);
-}
